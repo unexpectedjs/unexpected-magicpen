08d7bf6f039f73e02e42193df8ddb65db98c3d6d
Andreas Lind <andreas.lind@peakon.com>
2020-05-10 00:57:10 +0200

    prettier --write '**/*.{js,md}'

--- rollup.config.js
+++ rollup.config.js
@@ -3,33 +3,29 @@ const commonjs = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
 const json = require('rollup-plugin-json');
 
-const plugins = [
-    commonjs(),
-    resolve(),
-    json()
-];
+const plugins = [commonjs(), resolve(), json()];
 
 module.exports = [
-    {
-        input: pkg.main,
-        output: {
-            file: 'unexpected-magicpen.min.js',
-            name: 'weknowhow.unexpectedMagicPen',
-            format: 'umd',
-            exports: 'named',
-            legacy: true,
-            strict: false
-        },
-        plugins
+  {
+    input: pkg.main,
+    output: {
+      file: 'unexpected-magicpen.min.js',
+      name: 'weknowhow.unexpectedMagicPen',
+      format: 'umd',
+      exports: 'named',
+      legacy: true,
+      strict: false,
     },
-    {
-        input: pkg.main,
-        output: {
-            file: 'unexpected-magicpen.esm.js',
-            format: 'esm',
-            exports: 'named',
-            strict: false
-        },
-        plugins
-    }
+    plugins,
+  },
+  {
+    input: pkg.main,
+    output: {
+      file: 'unexpected-magicpen.esm.js',
+      format: 'esm',
+      exports: 'named',
+      strict: false,
+    },
+    plugins,
+  },
 ];
